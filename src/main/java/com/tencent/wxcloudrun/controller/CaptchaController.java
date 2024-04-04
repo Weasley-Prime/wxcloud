7ea5abb091116285aece730062dfe324ee9a69a5
weasley <1422971650@qq.com>
2024-04-04 12:55:10 +0800

    post消息

--- src/main/java/com/tencent/wxcloudrun/controller/CaptchaController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CaptchaController.java
@@ -3,7 +3,9 @@ package com.tencent.wxcloudrun.controller;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.model.Captcha;
 import com.tencent.wxcloudrun.service.CaptchaService;
+import com.tencent.wxcloudrun.tools.MessageTools;
 import com.tencent.wxcloudrun.vo.GetXmlMessageVo;
+import org.dom4j.DocumentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -13,7 +15,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.tencent.wxcloudrun.tools.SHA1;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Map;
 import java.util.Random;
 
 @RestController
@@ -68,15 +77,62 @@ public class CaptchaController {
 //        return "不一样";
     }
 
-    @PostMapping(value = "/checkSignature",consumes = {MediaType.TEXT_XML_VALUE},produces = {MediaType.APPLICATION_JSON_VALUE})
-    public String checkSignaturePost(@RequestBody() GetXmlMessageVo vo) {
+    @PostMapping(value = "/checkSignature",consumes = {MediaType.APPLICATION_XML_VALUE},produces = {MediaType.TEXT_XML_VALUE})
+    public void checkSignaturePost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         logger.info("/checkSignaturePost get request");
         logger.info("================================================================");
-        logger.info(vo.toString());
-//        if(sha1 == echostr){
-        return vo.toString();
-//        }
-//        return "不一样";
-    }
+        request.setCharacterEncoding("UTF-8");
 
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = response.getWriter();
+
+        String str = null;
+
+        try {
+//将request请求，传到Message工具类的转换方法中，返回接收到的Map对象
+
+            Map<String,String> map = MessageTools.xmlToMap(request);
+
+//从集合中，获取XML各个节点的内容
+
+            String ToUserName = map.get("ToUserName");
+
+            String FromUserName = map.get("FromUserName");
+
+            String CreateTime = map.get("CreateTime");
+
+            String MsgType = map.get("MsgType");
+
+            String Content = map.get("Content");
+
+            String MsgId = map.get("MsgId");
+
+            if(MsgType.equals("text")) {//判断消息类型是否是文本消息(text)
+
+                GetXmlMessageVo message = new GetXmlMessageVo();
+
+                message.setFromUserName(ToUserName);//原来【接收消息用户】变为回复时【发送消息用户】
+
+                message.setToUserName(FromUserName);
+
+                message.setMsgType("text");
+
+                message.setCreateTime(new Date().getTime());//创建当前时间为消息时间
+
+                message.setContent("您好，" + FromUserName + "\n我是：" + ToUserName
+
+                        + "\n您发送的消息类型为：" + MsgType + "\n您发送的时间为" + CreateTime
+
+                        + "\n我回复的时间为：" + message.getCreateTime() + "您发送的内容是" + Content);
+
+                str = MessageTools.objectToXml(message); //调用Message工具类，将对象转为XML字符串
+            }
+
+        } catch (DocumentException e){
+            e.printStackTrace();
+        }
+        out.print(str);
+        out.close();
+    }
 }
--- src/main/java/com/tencent/wxcloudrun/vo/GetXmlMessageVo.java
+++ src/main/java/com/tencent/wxcloudrun/vo/GetXmlMessageVo.java
@@ -1,25 +1,24 @@
 package com.tencent.wxcloudrun.vo;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
 import lombok.Data;
-@JacksonXmlRootElement(localName = "xml")
+
 @Data
 public class GetXmlMessageVo {
-    @JacksonXmlProperty(localName = "toUserName")
+
     String toUserName;
-    @JacksonXmlProperty(localName = "fromUserName")
+
     String fromUserName;
-    @JacksonXmlProperty(localName = "createTime")
-    Integer createTime;
-    @JacksonXmlProperty(localName = "msgType")
+
+    Long createTime;
+
     String msgType;
-    @JacksonXmlProperty(localName = "content")
+
     String content;
-    @JacksonXmlProperty(localName = "msgId")
-    Integer msgId;
-    @JacksonXmlProperty(localName = "msgDataId")
+
+    String msgId;
+
     String msgDataId;
-    @JacksonXmlProperty(localName = "idx")
-    Integer idx;
+
+    String idx;
 }
